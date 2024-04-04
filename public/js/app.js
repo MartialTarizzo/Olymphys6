98435f8ed4aab0b9e47774c2fa4cd015d018b360
alain <alain.jouve@wanadoo.fr>
2024-04-04 10:23:52 +0200

    comit du 4 avril

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ function changejure(j) {//j est l'objet input qui a lancé la fonction, pour le 
     var data_value = j.value;
     var data_type = j.name;
     var id_jure = j.id.split(data_type)[1];
-
     var formURL = document.getElementsByTagName('form')[2].action;
 
     $.ajax({
@@ -23,18 +22,42 @@ function changejure(j) {//j est l'objet input qui a lancé la fonction, pour le 
 
 }
 
-function changeequipe(e, i, j) {
-    var data_value = e.value;
-    var id_equipe = i;
-    var id_jure = j;
-    console.log(e.value);
-    console.log(i);
-    console.log(j);
-    var formURL = document.getElementsByTagName('form')[2].action;
+function changejurecn(j) {//j est l'objet input qui a lancé la fonction, pour le formulaire de gestion des jures des cia
+
+    var data_value = j.value;
+    var data_type = j.name;
+    var id_jure = j.id.split(data_type)[1];
+    var formURL = document.getElementsByTagName('form')[3].action;
+
     $.ajax({
         url: formURL,
         type: "POST",
-        data: {value: data_value, idequipe: id_equipe, idjure: id_jure},
+        data: {value: data_value, type: data_type, idjure: id_jure},
+
+        success: function () {
+            document.querySelector('#gestionjures').click()
+        },
+
+        error: function (data) {
+            alert("Error while submitting Data");
+        },
+    });
+
+
+}
+
+function changeequipe(e, i, j) {
+    var type = 'equipe';
+    var data_value = e.value;
+    var id_equipe = i;
+    var id_jure = j;
+    console.log(data_value);
+    var formURL = document.getElementsByName('forme'.concat(id_equipe))[0].action;
+    console.log(formURL);
+    $.ajax({
+        url: formURL,
+        type: "GET",
+        data: {type: type, value: data_value, idequipe: id_equipe, idjure: id_jure},
 
         success: function () {
             document.querySelector('#gestionjures').click()
@@ -55,6 +78,22 @@ $('#modalconfirmjure').on('show.bs.modal', function (event) {
 
     var modal = $(this)
     modal.find('.modal-title').text('Attention!!!!')
+    modal.find('.modal-body input').val(recipient)
+});
+$('#modalconfirmjurecn').on('show.bs.modal', function (event) {//envoi des conseils jury cia
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('idjure');
+
+    var modal = $(this)
+    modal.find('.modal-title').text('Attention!!!!')
+    modal.find('.modal-body input').val(recipient)
+});
+$('#modalenvoiconseilscn').on('show.bs.modal', function (event) {//envoi des recommandations jury du cn
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('id');
+    console.log(recipient)
+    var modal = $(this)
+    modal.find('.modal-title').text('Attention')
     modal.find('.modal-body input').val(recipient)
 });
 $('#modalenvoiconseils').on('show.bs.modal', function (event) {
@@ -133,7 +172,10 @@ function modifsalle(j) {//j est l'objet input qui a lancé la fonction, pour le 
     var id_equipe = j.id.split(data_type)[1];
 
     var formURL = document.getElementById('formsalles').action;
+    console.log(data_type);
+    console.log(id_equipe);
 
+    console.log(data_value);
 
     $.ajax({
         url: formURL,
@@ -152,4 +194,32 @@ function modifsalle(j) {//j est l'objet input qui a lancé la fonction, pour le 
 
 }
 
+function modifordre(j) {//j est l'objet input qui a lancé la fonction, pour le formulaire de gestion des jures des cia
+
+    var data_value = j.value;
+    var data_type = j.name;
+    var id_equipe = j.id.split(data_type)[1];
+
+    var formURL = document.getElementById('formordre').action;
+    console.log(data_type);
+    console.log(id_equipe);
+
+    console.log(data_value);
+
+    $.ajax({
+        url: formURL,
+        type: "GET",
+        data: {value: data_value, type: data_type, idequipe: id_equipe},
+
+        success: function () {
+            document.querySelector('#gestionjures').click()
+        },
+
+        error: function (data) {
+            alert("Error while submitting Data");
+        },
+    });
+
+
+}
 
