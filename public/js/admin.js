1cc30b04134ccb3e00199a6e53fded81d5d79f47
alain <alain.jouve@wanadoo.fr>
2024-05-02 18:29:04 +0200

    comit du 2 mai

---
+++ public/js/admin.js
@@ -0,0 +1,13 @@
+//Pour que les noms de fichiers s'affichent dans le champ de choix de fichier lors d'un upload
+var selectElement=null;
+window.onload = function() {
+    selectElement = document.getElementById("OdpfEditionsPassees_affiche_file");
+    selectElement.addEventListener("change", (event) => {
+        let inputFile = event.currentTarget;
+        $(inputFile).parent().find('.custom-file-label').html(inputFile.files[0].name);
+        var sizeFile=document.getElementsByClassName('input-group-text')
+        //$(sizeFile[1]).html(inputFile.files[0].size);
+
+
+    });
+}
