a0dc923bed1a71f2dfc39c2c7b9ee6bf1c9d4049
alain-ubuntu <alain.jouve@wanadoo.fr>
2025-02-07 08:48:45 +0100

    Pour que la modification d'un élève ne déclenche pas une erreur de conversion

--- src/Entity/Fichiersequipes.php
+++ src/Entity/Fichiersequipes.php
@@ -64,6 +64,11 @@ class Fichiersequipes //extends BaseMedia
         return $this->fichierFile;
     }
 
+    public function __toString(): string
+    {
+
+        return $this->fichier;
+    }
     /**
      * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
      * of 'UploadedFile' is injected into this setter to trigger the update. If this
